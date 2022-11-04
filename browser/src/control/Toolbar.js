5310edfa1efe40c2ed0233ee6b4647a3ffdce300
Pranam Lashkari <lpranam@collabora.com>
2022-11-04 07:02:56 +0530

    segregate edit/read only permissions better
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I85861352929d150aaf93da5570bda960b1ad46f0

--- browser/src/control/Toolbar.js
+++ browser/src/control/Toolbar.js
@@ -188,7 +188,7 @@ L.Map.include({
 	applyFont: function (fontName) {
 		if (!fontName)
 			return;
-		if (this.isPermissionEdit()) {
+		if (this.isEditMode()) {
 			var msg = 'uno .uno:CharFontName {' +
 				'"CharFontName.FamilyName": ' +
 					'{"type": "string", "value": "' + fontName + '"}}';
@@ -197,7 +197,7 @@ L.Map.include({
 	},
 
 	applyFontSize: function (fontSize) {
-		if (this.isPermissionEdit()) {
+		if (this.isEditMode()) {
 			var msg = 'uno .uno:FontHeight {' +
 				'"FontHeight.Height": ' +
 				'{"type": "float", "value": "' + fontSize + '"}}';
@@ -282,7 +282,7 @@ L.Map.include({
 			this.fire('error', {cmd: 'setStyle', kind: 'incorrectparam'});
 			return;
 		}
-		if (this.isPermissionEdit()) {
+		if (this.isEditMode()) {
 			var msg = 'uno .uno:StyleApply {' +
 					'"Style":{"type":"string", "value": "' + style + '"},' +
 					'"FamilyName":{"type":"string", "value":"' + familyName + '"}' +
@@ -296,7 +296,7 @@ L.Map.include({
 			this.fire('error', {cmd: 'setLayout', kind: 'incorrectparam'});
 			return;
 		}
-		if (this.isPermissionEdit()) {
+		if (this.isEditMode()) {
 			var msg = 'uno .uno:AssignLayout {' +
 					'"WhatPage":{"type":"unsigned short", "value": "' + this.getCurrentPartNumber() + '"},' +
 					'"WhatLayout":{"type":"unsigned short", "value": "' + layout + '"}' +
@@ -382,14 +382,14 @@ L.Map.include({
 			return;
 		if (this.dialog.hasOpenedDialog() && !command.startsWith('.uno:ToolbarMode'))
 			this.dialog.blinkOpenDialog();
-		else if (this.isPermissionEdit() || isAllowedInReadOnly) {
+		else if (this.isEditMode() || isAllowedInReadOnly) {
 			if (!this.messageNeedsToBeRedirected(command))
 				app.socket.sendMessage('uno ' + command + (json ? ' ' + JSON.stringify(json) : ''));
 		}
 	},
 
 	toggleCommandState: function (unoState) {
-		if (this.isPermissionEdit()) {
+		if (this.isEditMode()) {
 			if (!unoState.startsWith('.uno:')) {
 				unoState = '.uno:' + unoState;
 			}
