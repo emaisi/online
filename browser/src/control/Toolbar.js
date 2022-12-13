fed771fdce9c18347baa47762d5a420cc46b6e80
Andras Timar <andras.timar@collabora.com>
2022-12-13 11:21:55 +0100

    Add '(unbranded)' string to product name, when branding is missing
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I231f06f509c2d70ba092658b828e1d201cef2887

--- browser/src/control/Toolbar.js
+++ browser/src/control/Toolbar.js
@@ -429,7 +429,7 @@ L.Map.include({
 		if (window.ThisIsAMobileApp) {
 			productName = window.MobileAppName;
 		} else {
-			productName = (typeof brandProductName !== 'undefined') ? brandProductName : 'Collabora Online Development Edition';
+			productName = (typeof brandProductName !== 'undefined') ? brandProductName : 'Collabora Online Development Edition (unbranded)';
 		}
 		var w;
 		var iw = window.innerWidth;
@@ -603,7 +603,7 @@ L.Map.include({
 		if (window.ThisIsAMobileApp) {
 			productName = window.MobileAppName;
 		} else {
-			productName = (typeof brandProductName !== 'undefined') ? brandProductName : 'Collabora Online Development Edition';
+			productName = (typeof brandProductName !== 'undefined') ? brandProductName : 'Collabora Online Development Edition (unbranded)';
 		}
 		var productURL = (typeof brandProductURL !== 'undefined') ? brandProductURL : 'https://collaboraonline.github.io/';
 		content.find('#product-name').text(productName).addClass('product-' + productName.split(/[ ()]+/).join('-').toLowerCase());
